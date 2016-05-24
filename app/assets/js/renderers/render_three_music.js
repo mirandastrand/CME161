003770fef3e918ed840e7107c1b80395ce8d48b9
mirandastrand <mstrand@stanford.edu>
2016-05-23 19:29:07 -0700

    audio processing

--- app/assets/js/renderers/render_three_music.js
+++ app/assets/js/renderers/render_three_music.js
@@ -11,14 +11,47 @@ var constraints = {
 };
 
 function successCallback(stream) {
-    console.log("success");
+    /*console.log("success");
     //console.log(stream);
     //console.log(stream.getAudioTracks());
     var left = stream.inputBuffer.getChannelData (0);
     var right = stream.inputBuffer.getChannelData (1);
     console.log(left);
-    console.log(right);
-}
+    console.log(right);*/
+
+    audioContext = new webkitAudioContext();
+    analyser = audioContext.createAnalyser();
+    microphone = audioContext.createMediaStreamSource(stream);
+    javascriptNode = audioContext.createJavaScriptNode(2048, 1, 1);
+
+    analyser.smoothingTimeConstant = 0.3;
+    analyser.fftSize = 1024;
+
+    microphone.connect(analyser);
+    analyser.connect(javascriptNode);
+    javascriptNode.connect(audioContext.destination);
+
+    //canvasContext = $("#canvas")[0].getContext("2d");
+    //canvasContext = document.getElementById("test");
+    //canvasContext= canvasContext.getContext("2d");
+
+    javascriptNode.onaudioprocess = function() {
+        var array =  new Uint8Array(analyser.frequencyBinCount);
+        analyser.getByteFrequencyData(array);
+        var values = 0;
+
+        var length = array.length;
+        for (var i = 0; i < length; i++) {
+            values += array[i];
+        }
+
+        var average = values / length;
+        //canvasContext.clearRect(0, 0, 60, 130);
+        //canvasContext.fillStyle = '#00ff00';
+        //canvasContext.fillRect(0,130-average,25,130);
+        console.log(average);
+    }
+}  
 
 function errorCallback(error) {
   console.log('navigator.getUserMedia error: ', error);
