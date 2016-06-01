1ffbbed85d5a9d3d2134b805dce12631aeb171fc
mirandastrand <mstrand@stanford.edu>
2016-06-01 01:31:57 -0700

    int

--- test.py
+++ test.py
@@ -1,6 +1,28 @@
-import os, copy, json
+import os, copy, json, collections
 from flask import Flask, jsonify, request, send_from_directory, make_response
 
+def make_data_graph(data_list_in):
+	idx = 0
+	names = collections.OrderedDict()
+	for e in data_list_in:
+		to = e['to'][:7] # truncate
+		fr = e['from'][:7]
+		if to not in names:
+			names[to] = idx
+			idx += 1
+		if fr not in names:
+			names[fr] = idx
+			idx += 1
+	edges = [{
+				"source": names[e['to'][:7]], 
+				"target": names[e['from'][:7]], 
+				"value": e['n'], 
+				"tags":  [d['tag'] for d in e['data']] 
+			} for e in data_list_in
+		]
+	nodes = [{"name":n} for n in names.keys()]
+	return { "nodes": nodes, "edges": edges }
+
 if __name__ == "__main__":
-	with open('app/assets/data/trellis.json') as data_file:	
-		print json.dumps(json.load(data_file))
+	with open('app/assets/data/trellis.json') as data_file:
+		print json.dumps(make_data_graph(json.load(data_file)[:2]))
