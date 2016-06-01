cc6bb1273bf453a779cd52762bada68116d3e9c0
mirandastrand <mstrand@stanford.edu>
2016-06-01 01:11:47 -0700

    debug

--- flask_front_end_app.py
+++ flask_front_end_app.py
@@ -23,9 +23,10 @@ def send_assets(path):
 
 @app.route('/trellis', methods=['GET'])
 def get_trellis():
-	with open('app/assets/data/trellis.json') as data_file:
-		return "[\"to\": \"from\"]"
-		//return json.dumps(json.load(data_file))
+	return "[\"to\": \"from\"]"
+	#with open('app/assets/data/trellis.json') as data_file:
+		# return "[\"to\": \"from\"]"
+		#return json.dumps(json.load(data_file))
 
 @app.route('/trellis/limit/', methods=['GET'])
 def get_trellis_limit(n_entries):
